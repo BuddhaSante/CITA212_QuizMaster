61e63ec4d02f9d23154f73202a35c01678eaba61
Chris Bonsante <166825974+BuddhaSante@users.noreply.github.com>
2024-10-23 13:42:48 -0400

    Completion Plus fixes
    Fixed Progress bar and other bugs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     Quiz quiz;
     EndScreen endScreen;
-    void Start()
+
+    void Awake()
     {
         quiz = FindAnyObjectByType<Quiz>();
         endScreen = FindObjectOfType<EndScreen>();
-
+    }
+    void Start()
+    {
         quiz.gameObject.SetActive(true);
         endScreen.gameObject.SetActive(false);
     }
@@ -18,6 +22,16 @@ public class GameManager : MonoBehaviour
     
     void Update()
     {
-        
+       if(quiz.isComplete)
+       {
+        quiz.gameObject.SetActive(false);
+        endScreen.gameObject.SetActive(true);
+        endScreen.ShowFinalScore();
+       } 
+    }
+
+    public void OnReplayLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
